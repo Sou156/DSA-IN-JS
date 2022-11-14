c64f763ff21b52dc9f53f813ffc39d0b2ec35189
Soumili Chatterjee <riyachatterjee403@gmail.com>
2022-11-14 09:22:25 +0530

    Truncate Sentence Problem No:1816

---
+++ truncateSentence.js
@@ -0,0 +1,24 @@
+// Leetcode Question Number:1816 (Truncate Sentence)
+
+// Question
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+// For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+// You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+// Sample Test Case 
+
+// Input: s = "Hello how are you Contestant", k = 4
+// Output: "Hello how are you"
+// Explanation:
+// The words in s are ["Hello", "how" "are", "you", "Contestant"].
+// The first 4 words are ["Hello", "how", "are", "you"].
+// Hence, you should return "Hello how are you".
+
+
+var truncateSentence = function(s, k) {
+  let str = s.split(" ").slice(0,k)
+  return str.join(" ")
+};
+
+console.log(truncateSentence( "What is the solution to this problem", 4))
