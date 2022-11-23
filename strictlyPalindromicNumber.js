f070dc5eee4a4a3c896ed035247d1521c3bab662
Soumili Chatterjee <riyachatterjee403@gmail.com>
2022-11-23 12:38:28 +0530

    Leetcode Question number 2396

---
+++ strictlyPalindromicNumber.js
@@ -0,0 +1,30 @@
+// Question Number =2396 
+
+// An integer n is strictly palindromic if, for every base b between 2 and n - 2 (inclusive), the string representation of the integer n in base b is palindromic.
+
+// Given an integer n, return true if n is strictly palindromic and false otherwise.
+
+// A string is palindromic if it reads the same forward and backward.
+
+
+// Sample Input 1 
+
+// Input: n = 9
+// Output: false
+// Explanation: In base 2: 9 = 1001 (base 2), which is palindromic.
+// In base 3: 9 = 100 (base 3), which is not palindromic.
+// Therefore, 9 is not strictly palindromic so we return false.
+// Note that in bases 4, 5, 6, and 7, n = 9 is also not palindromic.
+
+var isStrictlyPalindromic = function(n) {
+  for(let i=2; i<=n;i++){
+      if(n.toString(i).split("").reverse().join(" ")==n.toString(i)){
+          return true
+      }else{
+        return  false
+      }
+  }
+     
+  };
+
+  console.log(isStrictlyPalindromic(4))
