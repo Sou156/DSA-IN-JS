854c667ea74bf9abc4a2453e3160e763271fa2ab
Soumili Chatterjee <riyachatterjee403@gmail.com>
2022-12-17 21:33:06 +0530

    Question no 2413

---
+++ smallestEvenMultiple.js
@@ -0,0 +1,24 @@
+// Question no 2413
+
+// Question 
+
+// Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+
+// Example 1
+
+// Input: n = 5
+// Output: 10
+// Explanation: The smallest multiple of both 5 and 2 is 10.
+
+// Solution
+
+var smallestEvenMultiple = function(n) {
+  if(n%2==0){
+      return n
+  }else{
+      return n*2
+  }
+  };
+
+
+  console.log(smallestEvenMultiple(6))
