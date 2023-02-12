ba6b2c366c1fab9066834c665eda4f751fbdac2e
Soumili Chatterjee <riyachatterjee403@gmail.com>
2023-02-12 20:16:52 +0530

    Question no 2236 Leetcode

---
+++ RootEqualsSumofChildren.js
@@ -0,0 +1,21 @@
+// Question no 2236
+
+// You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+
+// Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+
+//  Input 1
+
+// Input: root = [10,4,6]
+// Output: true
+// Explanation: The values of the root, its left child, and its right child are 10, 4, and 6, respectively.
+// 10 is equal to 4 + 6, so we return true.
+
+// Solution 
+
+var checkTree = function(root) {
+  if (root.val === root.left.val + root.right.val) return true
+else return false
+};
+
+console.log(checkTree([10,4,6]));
