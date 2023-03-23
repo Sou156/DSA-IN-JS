6907b2e11d66c8cbbdf25ea4562506248451b115
Soumili Chatterjee <riyachatterjee403@gmail.com>
2023-03-23 20:41:58 +0530

    Question no 804 Leetcode

---
+++ uniqueMorseCodeWords.js
@@ -0,0 +1,49 @@
+// Question no 804
+
+// International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+// 'a' maps to ".-",
+// 'b' maps to "-...",
+// 'c' maps to "-.-.", and so on.
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+// [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+// Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+// For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+// Return the number of different transformations among all words we have.
+
+//  Example 1: 
+
+// Input: words = ["gin","zen","gig","msg"]
+// Output: 2
+// Explanation: The transformation of each word is:
+// "gin" -> "--...-."
+// "zen" -> "--...-."
+// "gig" -> "--...--."
+// "msg" -> "--...--."
+// There are 2 different transformations: "--...-." and "--...--.".
+
+
+var uniqueMorseRepresentations = function(words) {
+  let alphabets= 'abcdefghijklmnopqrstuvwxyz'.split("")
+let morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+let morseCodes = []
+for(let i=0;i<words.length;i++){
+  let code = ""
+  for(let j=0;j<words[i].length;j++){
+      let index = alphabets.indexOf(words[i][j])
+      code+=morse[index]
+
+  }
+  morseCodes.push(code)
+  code= ""
+}
+
+
+const unique = new Set(morseCodes)
+
+return unique.size
+
+};
